e526dcdeaf5aaddab8c312fa8c903eacb46f9624
vanx2 <happy_yatani@yahoo.co.jp>
2015-07-15 11:48:36 +0900

    clean up

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/bin/env node
 
 var detector = require('./lib/chromecastDetector.js');
-var Client                = require('castv2-client').Client;
+var Client   = require('castv2-client').Client;
 var CustomReciever = require('./lib/customReciever.js');
 
 // detect chromecast address in my network via multicast DNS
 detector.on('detect', function (cast){
-  if ( cast.name.substr(0,4) != 'vanx' ){ return; }
+//  if ( cast.name.substr(0,4) != 'vanx' ){ return; }
 //  if ( cast.name.substr(0,8) != 'signage@' ){ return; }
   console.log('[KICK] ', new Date().toLogFormat(), cast.name );
 
@@ -17,45 +17,77 @@ detector.on('detect', function (cast){
       (status.hasOwnProperty('applications') && status.applications[0].hasOwnProperty('displayName') ) ?
        status.applications[0].displayName : status);
   });
-  client.connect(cast.ip, function() {
+  client.connect(cast.ip, monitor);
+  function launch(){
+console.log('[launch]',new Date().toLogFormat(), cast.name);
     client.launch(CustomReciever, function(err, player) {
       if(err){ remove(err); } else {
-        player.on('status', function(status) {
+        client.player = player;
+        client.player.on('status', function(status) {
           console.log('[PLAYER STATUS]', new Date().toLogFormat(), cast.name,
             (status.hasOwnProperty('playerState')) ? status.playerState : status) ;
         });
-        player.load({contentId: 'http://192.168.1.171:8080/x.mp4'}, { autoplay: true }, function(err, status) {
-          if(err){ remove(err); } else {
-            client.clientTimer = setInterval(function() {
-              client.getStatus(function(err, status){
-                if(err) {
-                  remove(err);
-                } else if (status.hasOwnProperty('applications') && status.applications[0].appId === 'E8C28D3C'){
-                  remove('dropped'); 
-                } else if (status.hasOwnProperty('applications') && status.applications[0].appId == CustomReciever.APP_ID ) {
-                  player.getStatus(function(err, status){
-                    if(err){ remove(err);
-                    } else if ( status.hasOwnProperty('playerState') && 
-                                status.playerState != 'PLAYING' && status.playerState != 'BUFFERING' ) {
-                      console.log('[RELOAD] ', new Date().toLogFormat(), cast.name, status.playerState );
-//                      if(client.clientTimer){clearInterval(client.clientTimer);}
-                      player.load({contentId: 'http://192.168.1.171:8080/x.mp4'}, { autoplay: true },function (){}); // todo
-                    }
-                  });
-                }
-              });
-            }, 10000);
+        client.player.load({contentId: 'http://192.168.1.171:8080/x.mp4'}, { autoplay: true }, 
+          function(err, status) {
+            if(err){ remove(err); } else {
+              client.timer = setTimeout(monitor,3*1000);
+            }
           }
-        });
+        );
       }
     });
-  });
+  }
+  function monitor(){
+console.log('watch', new Date().toLogFormat(), cast.name);
+    client.getStatus(function(err, status){
+      if(err) {remove(err);}
+      if (status && status.hasOwnProperty('applications')){
+        if (status.applications[0].appId === 'E8C28D3C'){
+          console.log('backdrop!');
+          launch();
+        } else if (status.applications[0].appId == CustomReciever.APP_ID ) {
+          if (! client.player){
+console.log('join', new Date().toLogFormat(), cast.name);
+            client.join(status.applications[0], CustomReciever, function (err, player) {
+              if(err){ remove(err);}
+              client.player = player;
+              playerMonitor();
+            });
+          } else {
+            playerMonitor();
+          }
+        } else {
+          console.log('[IGNORE APP]', new Date().toLogFormat(), cast.name, status);
+          client.timer = setTimeout(monitor,3*1000);
+        }
+      } else {
+console.log('client has no status', new Date().toLogFormat(), cast.name);
+        client.timer = setTimeout(monitor,3*1000);
+      }
+    });
+  }
+  function playerMonitor(){
+    client.player.getStatus(function(err, status){
+      if(err){ remove(err);}
+      if ( status && status.hasOwnProperty('playerState')) { 
+        if (status.playerState != 'PLAYING' && status.playerState != 'BUFFERING' ) {
+          console.log('[RELOAD] ', new Date().toLogFormat(), cast.name, status.playerState );
+          launch();
+        } else {
+          console.log('[PLAYER STATUS]', new Date().toLogFormat(), cast.name, status.playerState); 
+          client.timer = setTimeout(monitor,3*1000);
+        }
+      } else {
+console.log('player has no status', new Date().toLogFormat(), cast.name);
+        client.timer = setTimeout(monitor,3*1000);
+      }
+    });
+  }
   function remove(err) {
     console.log('[ERROR]', new Date().toLogFormat(), cast.name, err);
-    if(client.clientTimer){clearInterval(client.clientTimer);}
-    try { if(client.receiver){ client.close();} } catch (err) {console.log(err);}
-    try { delete client;} catch (err) {console.log(err);}
-//    detector.emit('detect', cast);
+    if(client.timer){clearTimeout(client.timer);}
+    if(client.receiver){ client.close();}
+    delete client;
     delete detector.casts[cast.name];   // todo
   }
 });
