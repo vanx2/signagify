a31cc0b55ee911fd095c2b00f8fe29a0eaffd827
vanx2 <happy_yatani@yahoo.co.jp>
2015-09-08 11:09:51 +0900

    change path style on server.js

--- server.js
+++ server.js
@@ -4,31 +4,31 @@ var fs = require('fs');
 var path = require('path');
 var URL  = require('url');
 var mediaPath = '/Library/WebServer/Documents/img';
-var pathStr = '/';
 
 http.createServer(function (req, res) {
   var query = URL.parse(req.url, true).query;
   var cb = (query.hasOwnProperty('callback')) ? query.callback : null;
   var fileList = [];
   function getFiles( dir ) {
-    fs.readdirSync(  mediaPath + dir
+    fs.readdirSync( dir
 /*
     ).sort(
     function(a, b){
-      return fs.statSync(mediaPath + dir + pathStr + a).mtime.getTime() -
-             fs.statSync(mediaPath + dir + pathStr + b).mtime.getTime();
+      return fs.statSync( path.join(dir, a) ).mtime.getTime() -
+             fs.statSync( path.join(dir, b) ).mtime.getTime();
     }
 */
     ).forEach(function (file) {
-      if (fs.statSync( mediaPath + dir + pathStr + file).isDirectory()){
-        getFiles( dir + pathStr + file );
-      } else if (fs.statSync( mediaPath + dir + pathStr + file).isFile() &&
-                 /\.(txt|mp4|png|gif|jpe?g)$/.test(file)) {
-        fileList.push( dir.replace(RegExp(pathStr, 'g'), '/') + '/' + file);
+      var target = path.join(dir, file);
+      if (fs.statSync(target).isDirectory()){
+        getFiles(target);
+      } else if (fs.statSync(target).isFile() &&
+                 /^\.(txt|mp4|png|gif|jpe?g)$/.test(path.extname(target))) {
+        fileList.push( target.replace(mediaPath, "").split(path.sep).join('/') );
       }
     });
   }
-  getFiles( '' );
+  getFiles( mediaPath.replace(RegExp(path.sep + '$'), '') );
   res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
   res.end(cb ? cb + "(" + JSON.stringify(fileList) + ")"
                    : JSON.stringify(fileList));
