dd1c0e52d8e25b7561b019b2bb4b74041438c3ca
vanx2 <happy_yatani@yahoo.co.jp>
2015-09-14 12:34:17 +0900

    add cast code to receiver

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var detector = require('./lib/chromecastDetector.js');
 var Client   = require('castv2-client').Client;
 var CustomReciever = require('./lib/customReciever.js');
+var watchInterval = 10;
 
 // detect chromecast address in my network via multicast DNS
 detector.on('detect', function (cast){
@@ -38,12 +39,13 @@ console.log('join', new Date().toLog(), cast.name);
           } else {
             playerMonitor();
           }
-        } else if (status.applications[0].appId == '0F5096E8'){
-          console.log('  [GUEST USE]', new Date().toLog(), cast.name, status); 
-          client.timer = setTimeout(monitor,5*1000);
         } else {
-          console.log('  [IGNORE APP]', new Date().toLog(), cast.name, status);
-          client.timer = setTimeout(monitor,5*1000);
+          if (status.applications[0].appId == '0F5096E8'){
+            console.log('  [GUEST USE]', new Date().toLog(), cast.name, status); 
+          } else {
+            console.log('  [IGNORE APP]', new Date().toLog(), cast.name, status);
+          }
+          client.timer = setTimeout(monitor,watchInterval * 1000);
         }
       } else {
 console.log('client has no status', new Date().toLog(), cast.name);
@@ -61,7 +63,7 @@ console.log('client has no status', new Date().toLog(), cast.name);
           launch();
         } else {
           console.log('  [PLAYER STATUS]', new Date().toLog(), cast.name, status.playerState); 
-          client.timer = setTimeout(monitor,5*1000);
+          client.timer = setTimeout(monitor,watchInterval * 1000);
         }
       } else {
 console.log('player has no status', new Date().toLog(), cast.name, status);
@@ -82,7 +84,7 @@ console.log('[launch]',new Date().toLog(), cast.name);
         client.player.load({contentId: 'http://192.168.1.171/x.mp4'}, { autoplay: true }, 
           function(err, status) {
             if(err){ remove(err); } else {
-              client.timer = setTimeout(monitor,5*1000);
+              client.timer = setTimeout(monitor, watchInterval *1000);
             }
           }
         );
