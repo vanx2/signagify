5c73b79fcd31f43b10ad818c3c273d173bab66c6
vanx2 <happy_yatani@yahoo.co.jp>
2016-06-10 19:24:32 +0900

    renew

--- filelist.js
+++ filelist.js
@@ -27,6 +27,7 @@ https.createServer(options, function (req, res) {
   
     var cb = (query.hasOwnProperty('callback')) ? query.callback : null;
     var fileList = [];
+    var textList = [];
     function getFiles( dir ) {
       fs.readdirSync( dir
 /*
@@ -41,16 +42,37 @@ https.createServer(options, function (req, res) {
         if (fs.statSync(target).isDirectory()){
           getFiles(target);
         } else if (fs.statSync(target).isFile() &&
-                   /^\.(txt|mp4|png|gif|jpe?g)$/i.test(path.extname(target))) {
-          fileList.push(
-            target.replace(config.fileList.mediaPath + path.sep, "").split(path.sep).join('/') );
+                   /^\.(txt|mp4|mpeg|png|gif|jpe?g)$/i.test(path.extname(target))) {
+          var url = target.replace(config.fileList.mediaPath + path.sep, "").split(path.sep).join('/');
+          if ( /^\.(txt)$/i.test(path.extname(file)) ) {
+            textList.push( fs.readFileSync(target).toString() );
+//              { "name": file.replace(/\.txt$/i, ""), 
+//                "text": fs.readFileSync(target).toString() }
+//            );
+          } else if (/^\.(mp4|mpe?g)$/i.test(path.extname(file)) ){
+            fileList.push(
+              { "type": "VIDEO",
+                "url": url}
+            );
+          } else if (/^\.(png|gif|jpe?g)$/i.test(path.extname(file))) {
+            var sec = file.split(/\./)[file.split(/\./).length - 2];
+            fileList.push(
+              { "type": "IMAGE",
+                "url": url,
+                "sec": ( ( isFinite(sec) )  ? ( 0 + sec ) : 10 )
+              }
+            );
+          }
         }
       });
     }
     getFiles( config.fileList.mediaPath.replace(RegExp(path.sep + '$'), '') );
     res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
-    res.end(cb ? cb + "(" + JSON.stringify(fileList) + ")"
-                     : JSON.stringify(fileList));
+    var body = JSON.stringify(
+      fileList ,  textList 
+    );
+    res.end( ( (cb) ? cb + "(" : "" ) + JSON.stringify(fileList) + "," + JSON.stringify(textList) +
+             ( (cb) ? ")" : "" ) );
   } else {
     var done = finalhandler(req, res);
     serve(req, res, done);
