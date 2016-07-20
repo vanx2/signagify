cee3b4b3d51c1a64bdc7519e2dd4f1c126b956a6
v <vanxx2@gmail.com>
2016-07-20 18:47:47 +0900

    renew

--- filelist.js
+++ filelist.js
@@ -1,27 +1,25 @@
 #!/bin/env node
-var config = require('config');
-var https = require('https');
+// var config = require('config');
+var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var URL  = require('url');
 var finalhandler = require('finalhandler')
 var serveStatic = require('serve-static')
+var port = 9000;
+var mediaPath = "z:\\";
 
-var mediaPath = '/Library/WebServer/Documents/img';
-//var mediaPath = 'Z:\\投稿受付';
-var mediaPath = 'C:\\Users\\dig_signage\\Desktop\\Contents\\img';
-var mediaPath = '/Users/van/Documents/work/signagify/pub';
-var mediaPath = '/var/apps/signagify/pub';
-
+/*
 var options = {
   key:    fs.readFileSync(config.ssl.key),
   cert:   fs.readFileSync(config.ssl.cert)
 };
-
 if(config.ssl.ca){options.ca = fs.readFileSync(config.ssl.ca);}
 var serve = serveStatic(config.fileList.mediaPath);
+*/
 
-https.createServer(options, function (req, res) {
+http.createServer(function (req, res) {
+console.log(req.url);
   if ( URL.parse(req.url).pathname === "/" ) {
     var query = URL.parse(req.url, true).query;
   
@@ -43,7 +41,7 @@ https.createServer(options, function (req, res) {
           getFiles(target);
         } else if (fs.statSync(target).isFile() &&
                    /^\.(txt|mp4|mpeg|png|gif|jpe?g)$/i.test(path.extname(target))) {
-          var url = target.replace(config.fileList.mediaPath + path.sep, "").split(path.sep).join('/');
+          var url = target.replace(mediaPath + path.sep, "").split(path.sep).join('/');
           if ( /^\.(txt)$/i.test(path.extname(file)) ) {
             textList.push( fs.readFileSync(target).toString() );
 //              { "name": file.replace(/\.txt$/i, ""), 
@@ -66,7 +64,7 @@ https.createServer(options, function (req, res) {
         }
       });
     }
-    getFiles( config.fileList.mediaPath.replace(RegExp(path.sep + '$'), '') );
+    getFiles( mediaPath.replace(RegExp(path.sep + '$'), '') );
     res.writeHead(200, {'Content-Type':'application/json; charset=utf-8'});
     var body = JSON.stringify(
       fileList ,  textList 
@@ -77,5 +75,5 @@ https.createServer(options, function (req, res) {
     var done = finalhandler(req, res);
     serve(req, res, done);
   }
-}).listen(config.fileList.port);
+}).listen(port);
 
--- server.js
+++ server.js
@@ -1,62 +1,64 @@
 #!/bin/env node
+
 var detector = require('chromecast-detector');
 var Client   = require('castv2-client').Client;
 var CustomReciever = require('castv2-client').DefaultMediaReceiver;
-CustomReciever.APP_ID = 'CA38BAF1'; // my custome receiver
 CustomReciever.APP_ID = '5D393044'; // my custome receiver
 
 detector.on('detect', function (cast){
-  if ( cast.name.substr(0,4) != 'vnew' ){ return; }
-console.log(cast.name);
-  var client = new Client();
-  client.on('error', function(err){console.log(err);});
-  client.on('status', function (status){console.log(status);});
-  client.connect(cast.ip, firstContact);
-  function firstContact(){
-    client.getStatus(function (err, status){
-      if ( err ) {  remove(err); return; }
+  if ( cast.name.substr(0,7) != 'signage' ){ return; }
+console.log("[DETECT]", cast.name);
+  c = new Client();
+  c.on('error', function(err){remove(err);});
+  c.on('status', function (status){console.log(status);});
+  c.connect(cast.ip, checkApp);
+  function checkApp(){
+    delete c.timer;
+    if (! c) {remove("client is null"); return; }
+    c.getStatus(function (err, status){
+      if ( err ) { remove(err); return; }
       if (status && status.hasOwnProperty('applications')){
         if (status.applications[0].appId === 'E8C28D3C'){
-          client.launch(CustomReciever, function(err, player) {
-            if(err){ remove(err); } else {
-              true;
-            }
-          });
+          console.log('  [Backdrop]', new Date().toLog(), cast.name);
+          launch();
         } else if (status.applications[0].appId == CustomReciever.APP_ID ) {
-console.log("already running");
-            client.launch(CustomReciever, function(err, player) {
-              if(err){ remove(err); } else {
-                true;
-              }
-            });
-          true;
+          console.log('  [RUNNING]', new Date().toLog(), cast.name);
         } else {
           if (status.applications[0].appId == '0F5096E8'){
-            console.log('  [GUEST USE]', new Date().toLog(), cast.name, status);
+            console.log('  [GUEST USE]', new Date().toLog(), cast.name);
           } else {
-            client.launch(CustomReciever, function(err, player) {
-              if(err){ remove(err); } else {
-                true;
-              }
-            });
             console.log('  [IGNORE APP]', new Date().toLog(), cast.name, status);
           }
         }
       }
     });
+    c.timer = setTimeout(checkApp, 10 * 1000);
   }
-
+  function launch(){
+    c.launch(CustomReciever, function(err, player) {
+      if(err){ remove(err); } else {
+        c.receiver = player;
+      }
+    });
+  }
   function remove(err) {
     console.log('[ERROR]', new Date().toLog(), cast.name, err);
-    if(client.timer){clearTimeout(client.timer);}
-    if(client.receiver){ client.close();}
-    delete client;
+    try{
+      clearTimeout(c.timer);
+    } catch(e){
+      console.log(e);
+    }
+    try{
+      c.close();
+    } catch(e){
+      console.log(e);
+    }
+    delete c;
     delete detector.casts[cast.id];
-    console.log(detector.casts);
+console.log(detector.casts);
     detector.sendQuery();
   }
 });
-
 
 Date.prototype.toLog = function() {
   return this.toISOString().split('T')[0] + ' ' +  this.toLocaleTimeString();
